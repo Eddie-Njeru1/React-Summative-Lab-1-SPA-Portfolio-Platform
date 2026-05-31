9aba97876d4568bd210d0a00db6187ae56cbd8ad
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-05-31 23:21:18 +0300

    "Edited App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-// Import the child components into the parent App component which will pass down data as props.
+// Import the child components into the parent App which will pass down data as props.
 import React from "react";
 import AddProjects from "./components/AddProjects";
 import ProjectLists from "./components/ProjectLists";
 import SearchProjects from "./components/SearchProjects";
-// import useState hook to manage the states of the project components
+// import useState hook to manage the states of the components
 import { useState } from "react";
 
 function App() {
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState("")
 
   return (
     <div>
