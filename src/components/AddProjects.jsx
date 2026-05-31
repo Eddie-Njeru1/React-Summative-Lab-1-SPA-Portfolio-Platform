4365ac5a0a45402f30e9cd3a0ca2cc1e10a7a8b4
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-05-31 23:12:15 +0300

    "AddProjects edited"

---
+++ src/components/AddProjects.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+// import useState hook to manage the state of this component
+import {useState} from "react";
+
+// Handle the state of each input section
+function AddProject() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //Prevents refreshing the page
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+            id="title"
+            type="text"
+            placeholder="Type here"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            />
+
+            <input
+            id="description"
+            type="text"
+            placeholder="Type here"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+
+        </form>
+    )
+};
+
+export default AddProject;
---
+++ src/components/ProjectLists.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+// import useState hook to manage the state of this component
+import {useState} from "react";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default ProjectList;
